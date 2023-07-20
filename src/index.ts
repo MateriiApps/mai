8de924cfa13c6180f7808cf94634fcc73fd97c7e
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-07-20 12:06:28 -0700

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits as GatewayIntents, Options } from 'discord.js'
+import * as process from "process";
+
+const client = new SapphireClient({
+    defaultPrefix: ["v", "m", "~"],
+    loadMessageCommandListeners: true,
+    intents: [
+        GatewayIntents.Guilds,
+        GatewayIntents.GuildMembers,
+        GatewayIntents.GuildMessages,
+        GatewayIntents.MessageContent
+    ],
+    makeCache: Options.cacheWithLimits({
+        GuildMemberManager: 0,
+        MessageManager: 0,
+    })
+})
+
+await client.login(process.env.TOKEN);
