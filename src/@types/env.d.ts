ce77d114b776b1c7a567a444fc9723525aaae947
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-08-25 21:03:48 -0700

    chore: typings for env variables

---
+++ src/@types/env.d.ts
@@ -0,0 +1,12 @@
+// noinspection JSUnusedGlobalSymbols
+
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        DISCORD_TOKEN?: string;
+        GITHUB_TOKEN?: string;
+        OWNERS?: string;
+        ANDROIDX_CHANNEL?: string;
+        ANDROIDX_ROLE?: string;
+        AGE_REQUIREMENT?: string;
+    }
+}
